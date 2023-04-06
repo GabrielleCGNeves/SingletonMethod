e9de88d53f2f499348824cfc4228a7291bcbe1c4
GabrielleCGNeves <gabriellecarvalhoneves@gmail.com>
2023-04-06 14:14:39 -0300

    :recycle: Singletown Method in JS

---
+++ Singleton02.js
@@ -0,0 +1,31 @@
+class Singleton{
+
+    constructor(){
+        console.log("Entrou no Construtor - Analisa se a instancia é `null`");
+        if(Singleton.instance == null){
+            Singleton.instance = this;
+            this.value = 0;
+        }
+        return Singleton.instance;
+    }
+
+    increment(){
+        this.value += 1;
+        console.log(`Value: ${this.value}`);
+    }
+}
+
+// -----------------------------------------------
+// Cliente:
+
+console.log("------------------ Cliente ------------------");
+
+const s1 = new Singleton();
+const s2 = new Singleton();
+
+console.log(s1 == s2);
+
+s1.increment();
+s2.increment();
+s1.increment();
+s2.increment();
